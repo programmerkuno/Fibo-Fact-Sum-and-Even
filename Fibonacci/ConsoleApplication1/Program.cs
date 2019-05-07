56904b668df2a7429a73bdc7e9db54d58972f070
programmerkuno <49220002+programmerkuno@users.noreply.github.com>
2019-05-07 17:47:30 +0800

    Add files via upload

---
+++ Fibonacci/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter Any Number: ");
+            num = int.Parse(Console.ReadLine());
+            if(num % 2 == 0)
+            {
+                Console.Write("Number {0} is Even", num);
+            }
+            else
+            {
+                Console.Write("Number {0} is Odd", num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
